d666f4f0f15c97fb30e98acca8dfd240f9058b9d
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-25 03:14:02 +0000

    Search bar creation, textbox, attching event and filtration see filteredMovies

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -4,6 +4,8 @@ import MovieCard from "./MovieCard.jsx";
 
 const MoviesGrid = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
     useEffect(() => {
         //loading from the file.
         fetch("movies.json")
@@ -11,13 +13,25 @@ const MoviesGrid = () => {
             .then(data => setMovies(data));
     }, []); //2nd arg is an empty array, because we don't have any dependency,
     // only when this component is loaded this should execute.
+
+    const handleSearchChange = (evt) => {
+        setSearchTerm(evt.target.value);
+    }
+
+    const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    //In the input field value is set as {searchTerm}, this is for binding the value, initial display or re-rendering.
+    // If we don't do this, with each re-render this will become empty.
     return (
-        <div className="movies-grid">
-            {
-                movies.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))
-            }
+        <div>
+            <input type="text" placeholder="Search Movies..." className="search-input" value={searchTerm} onChange={handleSearchChange} />
+            <div className="movies-grid">
+                {
+                    filteredMovies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))
+                }
+            </div>
         </div>
     );
 }
