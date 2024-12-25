a886a22b3a5af188666b87aa97539dfaf723d8d5
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-25 01:52:19 +0000

    MoviesGrid with useEffect used to update state and loading movies from a data file. Looping through the list and setting up as a card to be displayed on UI, import concept look , interpolation and map function.

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -11,7 +11,20 @@ const MoviesGrid = () => {
     }, []); //2nd arg is an empty array, because we don't have any dependency,
     // only when this component is loaded this should execute.
     return (
-        <div>{movies.length}</div>
+        <div className="movies-grid">
+            {
+                movies.map((movie) => (
+                    <div className="movie-card" key={movie.id}>
+                        <img src={`images/${movie.image}`} alt={movie.title} />
+                        <div className="movie-card-info">
+                            <h3 className="movie-card-title">{movie.title}</h3>
+                            <p className="movie-card-genre">{movie.genre}</p>
+                            <p className="movie-card-rating">{movie.rating}</p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
 
