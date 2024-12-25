30f2d5882085b7006b66352d6d0cc8784bb81ebf
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-25 02:34:35 +0000

    How to do error handling in case image is missing, this applies generally in principle

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,9 +7,16 @@ function MovieCard(props) {
         movie: PropTypes.object.isRequired
     }
     const movie = props.movie;
+
+    //this is just another style of writing js functions
+    const handleError = (evt) => { //event that caused the error
+        evt.target.src = "images/default.jpg"; //Since this function is tied on img tag, so target is img,
+        // this line is essentially replacing the src tag with the default.jpg
+    }
+
     return (
         <div className="movie-card" key={movie.id}>
-            <img src={`images/${movie.image}`} alt={movie.title}/>
+            <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className="movie-card-info">
                 <h3 className="movie-card-title">{movie.title}</h3>
                 <p className="movie-card-genre">{movie.genre}</p>
