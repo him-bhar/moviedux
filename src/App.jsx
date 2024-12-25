040961f3396fe9dc081cc9d619ba65e033d8e5e7
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-25 23:06:30 +0000

    Defined component Watchlist for other page. Router setup in App. '/' will take us to MoviesGrid, and /watchlist will take us to watchlist page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './styles.css'
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import MoviesGrid from "./components/MoviesGrid.jsx";
+import Watchlist from "./components/Watchlist.jsx";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import MovieCard from "./components/MovieCard.jsx";
 
 function App() {
 
@@ -9,7 +12,21 @@ function App() {
       <div>
           <div className="container">
               <Header />
-              <MoviesGrid />
+              <Router>
+                  <nav>
+                      <ul>
+                          <li>
+                              <Link to="/">Home</Link>
+                              <Link to="/watchlist">Watchlist</Link>
+                          </li>
+                      </ul>
+                  </nav>
+
+                  <Routes>
+                      <Route path="/" element={<MoviesGrid />} ></Route>
+                      <Route path="/watchlist" element={<Watchlist />} ></Route>
+                  </Routes>
+              </Router>
           </div>
           <Footer />
       </div>
